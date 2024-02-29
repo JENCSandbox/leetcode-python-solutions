b826850177f99af3185dd429fd2d8cc362a9e66d
Juan Esteban <juanesteban.navarro@microfocus.com>
2024-02-29 11:09:39 -0600

    Add, more versions to run 1480 example

--- 1480/example_1480.py
+++ 1480/example_1480.py
@@ -6,3 +6,12 @@ def running_sum(numbers):
         result.append(accumulator)
     return result
 
+
+def runningSum_2(self, nums):
+    result = []
+    if len(nums) > 0:
+        result.append(nums[0])
+        for index in range(1, len(nums)):
+            result.append(nums[index] + result[index - 1])
+    return result
+
