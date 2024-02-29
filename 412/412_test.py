b5324dafde2719523dbd1507f4d466ccbeeb1563
Juan Esteban <juanesteban.navarro@microfocus.com>
2024-02-29 11:45:01 -0600

    Add, solution 412 problem

---
+++ 412/412_test.py
@@ -0,0 +1,12 @@
+import unittest
+from example_412 import fizzBuzz
+
+class Test412(unittest.TestCase):
+    def test_basic(self):
+        data = 3
+        expectedResult = ["1", "2", "Fizz"]
+        result = fizzBuzz(data)
+        self.assertEqual(result, expectedResult)
+
+if __name__ == '__main__':
+    unittest.main()
