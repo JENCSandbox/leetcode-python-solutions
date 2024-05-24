dd39117123d688c9fe7e24a1d3bff66be25a90f5
Juan Esteban <juanesteban.navarro@microfocus.com>
2024-05-23 20:41:57 -0600

    add, 2259

--- 2259 - Remove Digit From Number to Maximize Result/solution.py
+++ 2259 - Remove Digit From Number to Maximize Result/solution.py
@@ -5,17 +5,11 @@ class Solution(object):
         :type digit: str
         :rtype: str
         """
-        result = 0
-
+        result = "0"
         for i, letter in enumerate(number):
-            if letter == str(digit) and int(number[0:i]+number[i+1:]) > result:
-                result = int(number[0:i]+number[i+1:])
-
-        return str(result) if result > 0 else number
-
-
-
-
+            if letter == str(digit):
+                result = max((number[0:i]+number[i+1:]), result)
+        return result if result > "0" else number
 
 sol = Solution()
 print(sol.removeDigit("12345678", 4))
