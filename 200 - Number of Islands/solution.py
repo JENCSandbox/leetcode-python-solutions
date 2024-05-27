4066cabe45775f2c5e9aa4ae2b12ddd03eab7ebf
Juan Esteban <juanesteban.navarro@microfocus.com>
2024-05-27 11:25:09 -0600

    add, 200

--- 200 - Number of Islands/solution.py
+++ 200 - Number of Islands/solution.py
@@ -16,22 +16,22 @@ class Solution:
 
             while q:
                 row, col = q.popleft()
-                directions = [[1,0], [-1, 0], [0,1], [0,-1]]
+                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
                 for dr, dc in directions:
-                    r,c = row + dr, col + dc
+                    r, c = row + dr, col + dc
                     if (r in range(rows) and
                         c in range(cols) and
                         grid[r][c] == "1" and
                             (r, c) not in visit
                     ):
-                        q.append((r,c))
+                        q.append((r, c))
                         visit.add((r, c))
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == "1" and (r,c) not in visit:
-                    bfs(r,c)
+                if grid[r][c] == "1" and (r, c) not in visit:
+                    bfs(r, c)
                     isLands += 1
         return isLands
 
